0a160c21bf8211ab33648973a140eed64f0d8fdb
Fredy Camas <fredycamasdev@gmail.com>
2023-11-08 16:31:00 -0500

    added zoom in

--- src/utlis/motion.js
+++ src/utlis/motion.js
@@ -35,3 +35,22 @@ export const textVariant = (delay) => {
       },
     };
   };
+
+  export const zoomIn = (delay, duration) => {
+    return {
+      hidden: {
+        scale: 0,
+        opacity: 0,
+      },
+      show: {
+        scale: 1,
+        opacity: 1,
+        transition: {
+          type: "tween",
+          delay: delay,
+          duration: duration,
+          ease: "easeOut",
+        },
+      },
+    };
+  };
